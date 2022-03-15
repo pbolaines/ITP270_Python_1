c685bab8ffd2e84deb5c155ffad77e5549967879
pbolaines <98132625+pbolaines@users.noreply.github.com>
2022-03-15 23:48:18 +0000

    A lot.

--- main.py
+++ main.py
@@ -1,1 +1,77 @@
-print ("Hello World")
+#Append Size
+my_list =[23, 42, 108]
+my_list.append(3)
+print ("This is your list: " + str(my_list))
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+#Append Sum
+L1=[1,1,2]
+print ("This is your list: " + str(L1))
+num1= 1
+num2= 2
+sum= [1 + 2]
+for x in sum:
+  print (x)
+num1= 2
+num2= 3
+sum= [2 + 3]
+for x in sum:
+  print (x)
+num1= 3
+num2= 5
+sum= [3 + 5]
+for x in sum:
+  print (x)
+#Larger List
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+L1=[4,10,2,5]
+L2=[-10,2,5,10]
+N=1
+fir= (L1[-N:])
+sec= (L2[-N:])
+if fir >= sec:
+   print ("The last element of the list is : " + str(fir))
+else:
+   print ("The last element of the list is : " + str(sec))
+#More Than N
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+L1= [4, 10, 2, 5]
+L2= [-10, 2, 5, 10]
+L1_sorted = sorted(L1)
+L2_sorted = sorted(L2)
+List_sorted = sorted(L1_sorted + L2_sorted)
+print (List_sorted)
+#Combine Sort
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+L1= (2, 4, 6, 2, 3, 2, 1, 2)
+occu_num= L1.count(2)
+num= 3
+print ("{} number of times".format(occu_num))     
+if occu_num > num:
+ print (True)
+else: 
+ print (False) 
