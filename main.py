f296d63924b980b45bf88446b840bb76d1e59ec7
pbolaines <98132625+pbolaines@users.noreply.github.com>
2022-03-16 00:31:08 +0000

    The code.

--- main.py
+++ main.py
@@ -1,77 +1,48 @@
-#Append Size
-my_list =[23, 42, 108]
-my_list.append(3)
-print ("This is your list: " + str(my_list))
+hair_styles = ['High Fade' , 'Low Fade' , 'Mid Fade' , 'Clean Shave' , 'Spunk']
+High_Fade = 10
+print("""Weekly Report
+                             Carlys  Hair & Salon
+                         Your hair deserves the best!!""")
+
+print('')
+print('')
+while True:
+  print('Only authorized users can access the weekly report.')
+  password = input('Please enter the code: ')
+  if password == '9750':
+    print('')
+    print('This is the report.')
+    break
+  else:
+    print('You are not authorized.')
+
 print ('')
+prices = [15 , 20 , 15  , 30 , 40]
+print('Old prices: ' + '$' + str(prices) )
+total_price = 0
+last_week = [2 , 5 , 1 , 5, 3]
+total_price += sum(prices)
+print('')
+print("Total price for all the haircuts: " + "$" + str(total_price ) + "$")
+
+average_price = sum(prices) / len(prices)
+print('Average price: ' + '$' + str(average_price))
+
+print('')
+print('These are the new prices, we care for your hair more than you!!')
+new_prices = [prices -5 for prices in prices]
+print('New Prices: ' + '$' + str(new_prices))
+print('')
+
+total_revenue = 0
+for i in range(len(hair_styles)):
+  total_revenue = prices[i] * last_week[i]
+print('')
+print('Total revenue: ' + '$' + str(total_revenue) )
+print('')
+average_daily_revenue = int(total_revenue) / 7 
+print('Average daily revenue: '  + '$' + str(average_daily_revenue))
+
 print ('')
-print ('')
-print ('')
-print ('')
-#Append Sum
-L1=[1,1,2]
-print ("This is your list: " + str(L1))
-num1= 1
-num2= 2
-sum= [1 + 2]
-for x in sum:
-  print (x)
-num1= 2
-num2= 3
-sum= [2 + 3]
-for x in sum:
-  print (x)
-num1= 3
-num2= 5
-sum= [3 + 5]
-for x in sum:
-  print (x)
-#Larger List
-print ('')
-print ('')
-print ('')
-print ('')
-print ('')
-print ('')
-print ('')
-print ('')
-L1=[4,10,2,5]
-L2=[-10,2,5,10]
-N=1
-fir= (L1[-N:])
-sec= (L2[-N:])
-if fir >= sec:
-   print ("The last element of the list is : " + str(fir))
-else:
-   print ("The last element of the list is : " + str(sec))
-#More Than N
-print ('')
-print ('')
-print ('')
-print ('')
-print ('')
-print ('')
-print ('')
-print ('')
-L1= [4, 10, 2, 5]
-L2= [-10, 2, 5, 10]
-L1_sorted = sorted(L1)
-L2_sorted = sorted(L2)
-List_sorted = sorted(L1_sorted + L2_sorted)
-print (List_sorted)
-#Combine Sort
-print ('')
-print ('')
-print ('')
-print ('')
-print ('')
-print ('')
-print ('')
-print ('')
-L1= (2, 4, 6, 2, 3, 2, 1, 2)
-occu_num= L1.count(2)
-num= 3
-print ("{} number of times".format(occu_num))     
-if occu_num > num:
- print (True)
-else: 
- print (False) 
+cuts_under_30 = [new_prices[i] for i in range(len(new_prices)-1) if new_prices[i] < 30]
+print('These are the haircuts under $30: ' + str(cuts_under_30))
