a173764161dad10a1d8ab0753fcdcc098ea2f219
pbolaines <98132625+pbolaines@users.noreply.github.com>
2022-04-06 01:17:14 +0000

    None

--- main.py
+++
@@ -1,8 +0,0 @@
-L1=[5, 2, -10, -4, 4, 5]
-print ("This is your list: " + str(L1))
-L1sum=+ sum(L1)
-numsum= (L1[-4] + L1[-3]) /2
-if L1sum % 2 == 0:
-    print ("There are an even number of elements! " + str(numsum))
-else:
-    print("There are an odd number of elements!") 
